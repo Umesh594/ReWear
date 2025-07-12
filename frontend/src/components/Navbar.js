905d502d059222aa2401c6b8985d933941f94e07
Umesh594 <146110753+Umesh594@users.noreply.github.com>
2025-07-12 16:54:34 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,16 +2,13 @@ import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import AuthContext from '../context/AuthContext';
-
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
-
   const handleLogout = () => {
     logout();
     navigate('/');
   };
-
   return (
     <AppBar position="static">
       <Toolbar>
@@ -38,5 +35,4 @@ const Navbar = () => {
     </AppBar>
   );
 };
-
 export default Navbar;
